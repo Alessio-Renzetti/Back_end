f421ecec604164a28f33a499625b653404bef9cb
l.ciano <l.ciano@fatainformatica.it>
2024-07-31 15:40:54 +0200

    Sistemata entity admin

--- src/main/java/Entity_hotel/Admin.java
+++ src/main/java/Entity_hotel/Admin.java
@@ -7,5 +7,41 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "admin")
 public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id_admin")
+    private Integer id_admin;
+
+    @Column(name="Username_AD")
+    private String userNameAD;
+
+    @Column(name = "Password_AD")
+    private String password;
+
+    public Integer getId_admin() {
+        return id_admin;
+    }
+
+    public void setId_admin(Integer id_admin) {
+        this.id_admin = id_admin;
+    }
+
+    public String getUserNameAD() {
+        return userNameAD;
+    }
+
+    public void setUserNameAD(String userNameAD) {
+        this.userNameAD = userNameAD;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
