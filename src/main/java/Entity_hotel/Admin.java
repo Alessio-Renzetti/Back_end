7374e6242fb7e51a926fc4b902467c9a16e536cb
l.ciano <l.ciano@fatainformatica.it>
2024-07-31 15:09:12 +0200

    Aggiunta entity prenotazioneCamera

---
+++ src/main/java/Entity_hotel/Admin.java
@@ -0,0 +1,11 @@
+package Entity_hotel;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+public class Admin {
+}
---
+++ src/main/java/Entity_hotel/PrenotazioneCamera.java
@@ -0,0 +1,55 @@
+package Entity_hotel;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="prenotazione_camera")
+public class PrenotazioneCamera {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_prenotazione_camera")
+    private Integer idPrenotazioneCamera;
+
+    @ManyToOne
+    @JoinColumn(name = "FK_camera")
+    private Camera camera;
+
+    @ManyToOne
+    @JoinColumn(name = "FK_prenotazione")
+    private Prenotazione prenotazione;
+
+    @Column(name = "n_ospiti")
+    private Integer n_ospiti;
+
+    public Integer getIdPrenotazioneCamera() {
+        return idPrenotazioneCamera;
+    }
+
+    public void setIdPrenotazioneCamera(Integer idPrenotazioneCamera) {
+        this.idPrenotazioneCamera = idPrenotazioneCamera;
+    }
+
+    public Camera getCamera() {
+        return camera;
+    }
+
+    public void setCamera(Camera camera) {
+        this.camera = camera;
+    }
+
+    public Prenotazione getPrenotazione() {
+        return prenotazione;
+    }
+
+    public void setPrenotazione(Prenotazione prenotazione) {
+        this.prenotazione = prenotazione;
+    }
+
+    public Integer getN_ospiti() {
+        return n_ospiti;
+    }
+
+    public void setN_ospiti(Integer n_ospiti) {
+        this.n_ospiti = n_ospiti;
+    }
+}
