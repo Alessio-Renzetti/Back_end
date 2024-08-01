853a6ab954a025961d1d962180ec7d53327c343d
l.ciano <l.ciano@fatainformatica.it>
2024-08-01 15:16:27 +0200

    Sistemata entity cliente

--- src/main/java/com/example/demo/Entity_hotel/Cliente.java
+++ src/main/java/com/example/demo/Entity_hotel/Cliente.java
@@ -9,6 +9,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
+import java.util.Date;
+
 @Entity
 @Table(name="cliente")
 public class Cliente {
@@ -25,13 +27,13 @@ public class Cliente {
 	    private String nome_cliente;
 	    
 	    @Column(name="cognome_cliente")
-	    private Integer cognome_cliente;
+	    private String cognome_cliente;
 	    
 	    @Column(name="data_nascita")
-	    private Integer data_nascita;
+	    private Date data_nascita;
 	    
 	    @Column(name="password_CL")
-	    private Integer password_CL;
+	    private String password_CL;
 
 	    // Costruttori, getter e setter
 
@@ -50,15 +52,15 @@ public class Cliente {
 			return nome_cliente;
 		}
 
-		public Integer getCognome_cliente() {
+		public String getCognome_cliente() {
 			return cognome_cliente;
 		}
 
-		public Integer getData_nascita() {
+		public Date getData_nascita() {
 			return data_nascita;
 		}
 
-		public Integer getPassword_CL() {
+		public String getPassword_CL() {
 			return password_CL;
 		}
 
@@ -74,15 +76,15 @@ public class Cliente {
 			this.nome_cliente = nome_cliente;
 		}
 
-		public void setCognome_cliente(Integer cognome_cliente) {
+		public void setCognome_cliente(String cognome_cliente) {
 			this.cognome_cliente = cognome_cliente;
 		}
 
-		public void setData_nascita(Integer data_nascita) {
+		public void setData_nascita(Date data_nascita) {
 			this.data_nascita = data_nascita;
 		}
 
-		public void setPassword_CL(Integer password_CL) {
+		public void setPassword_CL(String password_CL) {
 			this.password_CL = password_CL;
 		}
 
