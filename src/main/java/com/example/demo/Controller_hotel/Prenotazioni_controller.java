10ef7b4ebfba9016264532ddcb13e2b4b0cd4187
Your Name <you@example.com>
2024-08-01 16:28:05 +0200

    commit per pull

--- src/main/java/com/example/demo/Controller_hotel/Prenotazioni_controller.java
+++ src/main/java/com/example/demo/Controller_hotel/Prenotazioni_controller.java
@@ -1,30 +1,41 @@
 package com.example.demo.Controller_hotel;
 
-
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import com.example.demo.Entity_hotel.Prenotazione;
+import com.example.demo.Service_hotel.Prenotazioni_service;
 
 
 @Controller
-
-
+@RequestMapping(value="prenotazioni")
 public class Prenotazioni_controller {
 	
-	@Autowired private Prenotazione_controller  Prenotazione_controller;
+	@Autowired
+	private Prenotazioni_service prenotazioniservice;
 	
-	public visualizza_lista() {
-		
-		
+	
+	@CrossOrigin(origins="*")
+	@ResponseBody
+	@RequestMapping(value="lista")
+	public String visualizza_lista(@RequestBody ) {
+		return null;
 	}
 	
 
+	@CrossOrigin(origins="*")
+	@ResponseBody
+	@RequestMapping(value="modifica_prenotazione")
+	public String modifica_Prenotazione() {
+		return null;}
 	
-	public modifica_Prenotazione() {}
 	
-	
-	
-	public elimina_Prenotazione() {}
+	@CrossOrigin(origins="*")
+	@ResponseBody
+	@RequestMapping(value="elimina_prenotazione")
+	public String elimina_Prenotazione() {
+		return null;}
 }
---
+++ src/main/java/com/example/demo/Service_hotel/Prenotazioni_service.java
@@ -0,0 +1,31 @@
+package com.example.demo.Service_hotel;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.clienteRepository.ClienteRepository;
+
+
+@Service
+public class Prenotazioni_service {
+
+	@Autowired  ClienteRepository clienterepository;
+	
+	public String visualizza_lista() {
+		
+		
+		
+		
+		return null;}
+	
+	
+	public String modifica_Prenotazione(){
+		return null;}
+	
+	
+	public String elimina_Prenotazione() {
+		return null;}
+}
+	
+	
+}
