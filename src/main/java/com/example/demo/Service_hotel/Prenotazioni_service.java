2cfc12824ff70a2cf17760bc22eb80e23247cb0f
Alessio-Renzetti <alessiorenzetti2004@gmail.com>
2024-09-04 15:52:13 +0200

    camere disponibili

--- src/main/java/com/example/demo/Service_hotel/Prenotazioni_service.java
+++ src/main/java/com/example/demo/Service_hotel/Prenotazioni_service.java
@@ -1,12 +1,13 @@
 package com.example.demo.Service_hotel;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.example.demo.Entity_hotel.Camera;
+import com.example.demo.Repository.Camera_repository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Service;
-
 import com.example.demo.Entity_hotel.Cliente;
 import com.example.demo.Entity_hotel.Prenotazione;
 import com.example.demo.Entity_hotel.PrenotazioneCamera;
@@ -17,12 +18,14 @@ import com.example.demo.dto_hotel.CredenzialiUtente;
 import com.example.demo.dto_hotel.Verifica_camere_DTO;
 
 
+
 @Service
 public class Prenotazioni_service {
 
 	@Autowired  ClienteRepository clienterepository;
 	@Autowired Prenotazionirepository prenotazionirepository;
-	@Autowired PrenotazioneCamera_repository prenotazioneCamera_repository; 
+	@Autowired PrenotazioneCamera_repository prenotazioneCamera_repository;
+	@Autowired Camera_repository camera_repository;
 	
 	public String visualizza_lista(CredenzialiUtente credenzialiUtente) {
 		Cliente cliente_autenticato = clienterepository.findByUsernameAndPassword(credenzialiUtente.getUsername(), credenzialiUtente.getPassword());
@@ -36,9 +39,15 @@ public class Prenotazioni_service {
 		
 		return null;}
 	
-		public String verifica_disponibilita(Verifica_camere_DTO verifica_camere) {
-			List<PrenotazioneCamera> camere_occupate = prenotazioneCamera_repository.verifica_disp(verifica_camere.getData_inizio(), verifica_camere.getData_fine());
-			return null;
+		public List<Camera> verifica_disponibilita(Verifica_camere_DTO verifica_camere) {
+			List<PrenotazioneCamera> app = prenotazioneCamera_repository.verifica_disp(verifica_camere.getData_inizio(), verifica_camere.getData_fine());
+			List<Camera> tutte = camera_repository.findAll();
+			List<Camera> camere_occupate = new ArrayList<Camera>();
+			for (int i=0;i<  app.size();i++){
+				camere_occupate.add(app.get(i).getCamera());
+			}
+			tutte.removeAll(camere_occupate);
+			return tutte;
 		}
 	
 	
